f0ff2f0939951676c1782613727d1ffc08ad8d54
shelger <yzhang433@dons.usfca.edu>
2023-06-01 01:00:37 -0700

    line

--- src/SharpEdgeDetection.py
+++ src/SharpEdgeDetection.py
@@ -1,30 +1,31 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
 from stl import mesh
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 # Load the STL file
-my_mesh = mesh.Mesh.from_file('../tests/withDraft.stl')
+my_mesh = mesh.Mesh.from_file('../tests/LinearMotionGuide.stl')
 
 
-def visualize_mesh(mesh, highlight_faces):
+def visualize_edges(mesh, highlight_edges):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    polygons = []
-    color = []
-    for i, v in enumerate(mesh.vectors):
-        polygons.append(v)
-        if i in highlight_faces:
-            color.append('r')
-        else:
-            color.append('b')
+    lines = []
+    for v in mesh.vectors:
+        for i in range(3):
+            lines.append(v[[i, (i+1)%3], :])
+    line_collection = Line3DCollection(lines, linewidths=0.3, colors='b', alpha=0.1)
 
-    collection = Poly3DCollection(polygons)
-    collection.set_facecolor(color)
-    ax.add_collection3d(collection)
+    highlight_lines = []
+    for edge in highlight_edges:
+        highlight_lines.append(edge)
+    highlight_line_collection = Line3DCollection(highlight_lines, linewidths=0.9, colors='r', alpha=1.0)
+
+    ax.add_collection3d(line_collection)
+    ax.add_collection3d(highlight_line_collection)
 
     scale = mesh.points.flatten()
     ax.auto_scale_xyz(scale, scale, scale)
@@ -40,19 +41,24 @@ def angle(v1, v2):
 
 def are_adjacent(face1, face2):
     """Returns True if the faces share exactly two vertices, False otherwise"""
-    shared_vertices = sum(any(np.array_equal(v, w) for w in face2) for v in face1)
-    return shared_vertices == 2
+    face1_set = set(map(tuple, face1))
+    face2_set = set(map(tuple, face2))
+    shared_vertices = face1_set & face2_set
+    if len(shared_vertices) == 2:
+        return np.array(list(shared_vertices))
+    else:
+        return None
 
 
 sharp_angle_threshold = np.radians(90)
-highlight_faces = []
+highlight_edges = []
 for i, normal in enumerate(my_mesh.normals):
     for j in range(i + 1, len(my_mesh.normals)):
-        if are_adjacent(my_mesh.vectors[i], my_mesh.vectors[j]):
-            # print(f"point between faces {my_mesh.vectors[i]} and  {my_mesh.vectors[j]}")
-            if angle(normal, my_mesh.normals[j]) >= sharp_angle_threshold:
-                print(f"The angle is {np.degrees(angle(normal, my_mesh.normals[j]))}")
-                print(f"Sharp edge found between faces {i} and {j}")
-                if i not in highlight_faces:
-                    highlight_faces.append(i)
-visualize_mesh(my_mesh, highlight_faces)
+        shared_vertices = are_adjacent(my_mesh.vectors[i], my_mesh.vectors[j])
+        if shared_vertices is not None:
+            # print(shared_vertices)
+            angle_between_normals = angle(normal, my_mesh.normals[j])
+            if angle_between_normals >= sharp_angle_threshold:
+                highlight_edges.append(shared_vertices)
+print(len(highlight_edges))
+visualize_edges(my_mesh, highlight_edges)
